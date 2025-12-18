7dc1b8bdb7d677603a382fb4f79a9483b80ff081
EldarJc <jusiceldar87@gmail.com>
2025-12-18 03:11:26 +0100

    nit DB, migrations, login/mail and register auth blueprint

--- src/__init__.py
+++ src/__init__.py
@@ -1,14 +1,25 @@
 from flask import Flask
 
-from .database import db, db_migrate
+from src.extensions import login_manager, mail
 
-def create_app(config):
+
+def create_app(config: type) -> Flask:
     if config is None:
-        raise ValueError("Configuration is required but was not provided.")
+        raise ValueError("Application configuration is missing")
 
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="ui/templates")
     app.config.from_object(config)
+
+    from .database import db, db_migrate
+
     db.init_app(app)
     db_migrate.init_app(app, db)
+
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from .auth import auth_bp
+
+    app.register_blueprint(auth_bp, url_prefix="/auth")
 
     return app
