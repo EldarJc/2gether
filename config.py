1a9ed62c3f5306eb805dcf1864c0ceeaef92e8ad
EldarJc <jusiceldar87@gmail.com>
2025-12-18 03:18:31 +0100

    add mail settings and db paths

--- config.py
+++ config.py
@@ -6,15 +6,30 @@ load_dotenv()
 
 base_dir = os.path.abspath(os.path.dirname(__file__))
 
+
 class BaseConfig:
     SECRET_KEY = os.getenv("DEV_SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI") + os.path.join(base_dir, "instance", "app_dev.db")
+    MAIL_SERVER = os.getenv("MAIL_SERVER")
+    MAIL_PORT = os.getenv("MAIL_PORT")
+    MAIL_USE_TLS = os.getenv("MAIL_USE_TLS")
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+    MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER")
+
 
 class DevConfig(BaseConfig):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI") + os.path.join(
+        base_dir, "instance", "app_dev.db"
+    )
+
 
 class TestConfig(BaseConfig):
+    WTF_CSRF_ENABLED = False
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI") + os.path.join(
+        base_dir, "instance", "app_test.db"
+    )
 
 
 # class ProductionConfig:
