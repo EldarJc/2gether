6a92b11a852017466ec73c4a953fec08d41de44e
EldarJc <jusiceldar87@gmail.com>
2025-12-23 21:57:56 +0100

    Add db helper functions

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+from .database import db
 from .database.models import User
 
 
@@ -17,4 +18,36 @@ def find_user(identifier: str) -> User | None:
 
 
 def get_user_by_id(user_id: int) -> User | None:
-    return User.query.get(user_id)
+    return db.session.get(User, user_id)
+
+
+def set_attributes(instance: object, attributes: dict) -> None:
+    for key, value in attributes.items():
+        if hasattr(type(instance), key):
+            setattr(instance, key, value)
+
+
+def save_instance(instance: object) -> bool:
+    try:
+        db.session.add(instance)
+        db.session.commit()
+        return True
+
+    except Exception:
+        db.session.rollback()
+        return False
+
+
+def update_instance(instance: object, attributes: dict) -> bool:
+    set_attributes(instance, attributes)
+    if db.session.is_modified(instance) or instance not in db.session:
+        return save_instance(instance)
+
+    return False
+
+
+def update_password(user_id: int, new_password: str) -> bool:
+    user = get_user_by_id(user_id)
+    if not user:
+        return False
+    return update_instance(user, {"password": new_password})
