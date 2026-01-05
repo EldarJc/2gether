0e0ab74050993c24bf7a165d40a37992efe47389
EldarJc <jusiceldar87@gmail.com>
2026-01-05 02:23:38 +0100

    Import models

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -3,3 +3,28 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 migrate = Migrate()
+
+
+from .models import (
+    Event,
+    EventAttendee,
+    Group,
+    GroupMember,
+    Location,
+    Tag,
+    User,
+    event_tags,
+    group_tags,
+)
+
+__all__ = [
+    "User",
+    "Group",
+    "Event",
+    "Tag",
+    "Location",
+    "GroupMember",
+    "EventAttendee",
+    "group_tags",
+    "event_tags",
+]
